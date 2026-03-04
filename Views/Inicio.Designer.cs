500d686bc9c9d89f0a6d06b6822dc4732d5ed244
Carlos Espinoza <cpespinoza044@gmail.com>
2026-03-03 21:56:48 -0600

    fix: revisando funcionamiento de git en wf

--- Views/Inicio.Designer.cs
+++ Views/Inicio.Designer.cs
@@ -34,7 +34,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(327, 204);
+            this.button1.Location = new System.Drawing.Point(324, 101);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
