6d0d35db6ea63b942d5bc26fdbde4db3f854b729
Javier Norori <javier.norori@est.ulsa.edu.ni>
2026-03-05 22:01:31 -0600

    Avance en Control de Clima

--- Views/Clima.Designer.cs
+++ Views/Clima.Designer.cs
@@ -28,16 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            FontAwesome.Sharp.IconPictureBox iconPictureBox1;
             this.panelRedondeado1 = new PanelRedondeado();
             this.panelRedondeado2 = new PanelRedondeado();
             this.panelRedondeado3 = new PanelRedondeado();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txt_control_de_clima = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.panelRedondeado1.SuspendLayout();
+            this.panelRedondeado2.SuspendLayout();
+            this.panelRedondeado3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(iconPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelRedondeado1
             // 
             this.panelRedondeado1.BackColor = System.Drawing.Color.Transparent;
             this.panelRedondeado1.ColorTarjeta = System.Drawing.Color.White;
+            this.panelRedondeado1.Controls.Add(iconPictureBox1);
+            this.panelRedondeado1.Controls.Add(this.label1);
+            this.panelRedondeado1.Controls.Add(this.panel1);
             this.panelRedondeado1.Location = new System.Drawing.Point(12, 69);
             this.panelRedondeado1.Name = "panelRedondeado1";
             this.panelRedondeado1.Padding = new System.Windows.Forms.Padding(8);
@@ -50,6 +68,8 @@
             // 
             this.panelRedondeado2.BackColor = System.Drawing.Color.Transparent;
             this.panelRedondeado2.ColorTarjeta = System.Drawing.Color.White;
+            this.panelRedondeado2.Controls.Add(this.label2);
+            this.panelRedondeado2.Controls.Add(this.panel3);
             this.panelRedondeado2.Location = new System.Drawing.Point(528, 69);
             this.panelRedondeado2.Name = "panelRedondeado2";
             this.panelRedondeado2.Padding = new System.Windows.Forms.Padding(8);
@@ -62,6 +82,8 @@
             // 
             this.panelRedondeado3.BackColor = System.Drawing.Color.Transparent;
             this.panelRedondeado3.ColorTarjeta = System.Drawing.Color.White;
+            this.panelRedondeado3.Controls.Add(this.label3);
+            this.panelRedondeado3.Controls.Add(this.panel4);
             this.panelRedondeado3.Location = new System.Drawing.Point(528, 266);
             this.panelRedondeado3.Name = "panelRedondeado3";
             this.panelRedondeado3.Padding = new System.Windows.Forms.Padding(8);
@@ -70,30 +92,119 @@
             this.panelRedondeado3.TabIndex = 2;
             this.panelRedondeado3.TamañoSombra = 6;
             // 
-            // linkLabel1
+            // panel1
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
-            this.linkLabel1.Location = new System.Drawing.Point(99, 19);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(147, 26);
-            this.linkLabel1.TabIndex = 3;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Control Clima";
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Location = new System.Drawing.Point(-1, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(494, 1);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.Location = new System.Drawing.Point(12, 41);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1043, 1);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gray;
+            this.panel3.Location = new System.Drawing.Point(0, 54);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(528, 1);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.Location = new System.Drawing.Point(0, 54);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(528, 1);
+            this.panel4.TabIndex = 2;
+            // 
+            // txt_control_de_clima
+            // 
+            this.txt_control_de_clima.AutoSize = true;
+            this.txt_control_de_clima.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_control_de_clima.Location = new System.Drawing.Point(22, 15);
+            this.txt_control_de_clima.Name = "txt_control_de_clima";
+            this.txt_control_de_clima.Size = new System.Drawing.Size(145, 24);
+            this.txt_control_de_clima.TabIndex = 3;
+            this.txt_control_de_clima.Text = "Control de Clima";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Termostato Principal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sensores Ambientales";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Acciones Rapidas";
+            // 
+            // iconPictureBox1
+            // 
+            iconPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            iconPictureBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Temperature4;
+            iconPictureBox1.IconColor = System.Drawing.SystemColors.ActiveCaptionText;
+            iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            iconPictureBox1.IconSize = 26;
+            iconPictureBox1.Location = new System.Drawing.Point(46, 17);
+            iconPictureBox1.Name = "iconPictureBox1";
+            iconPictureBox1.Size = new System.Drawing.Size(26, 28);
+            iconPictureBox1.TabIndex = 2;
+            iconPictureBox1.TabStop = false;
+            iconPictureBox1.Click += new System.EventHandler(this.iconPictureBox1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // Clima
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.txt_control_de_clima);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panelRedondeado3);
             this.Controls.Add(this.panelRedondeado2);
             this.Controls.Add(this.panelRedondeado1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Clima";
             this.Text = "Clima";
+            this.panelRedondeado1.ResumeLayout(false);
+            this.panelRedondeado1.PerformLayout();
+            this.panelRedondeado2.ResumeLayout(false);
+            this.panelRedondeado2.PerformLayout();
+            this.panelRedondeado3.ResumeLayout(false);
+            this.panelRedondeado3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(iconPictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +215,14 @@
         private PanelRedondeado panelRedondeado1;
         private PanelRedondeado panelRedondeado2;
         private PanelRedondeado panelRedondeado3;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label txt_control_de_clima;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
--- Views/Clima.cs
+++ Views/Clima.cs
@@ -16,5 +16,15 @@ namespace Domotica.Views
         {
             InitializeComponent();
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void iconPictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
