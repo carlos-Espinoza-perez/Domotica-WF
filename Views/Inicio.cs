288c64883abbf3f0578bb655b5a015c434b5d89a
Carlos Espinoza <cpespinoza044@gmail.com>
2026-03-06 08:41:42 -0600

    feat: Se finaliza la maquetacion de la vista de Inicio

--- Views/Inicio.cs
+++ Views/Inicio.cs
@@ -16,5 +16,25 @@ namespace Domotica.Views
         {
             InitializeComponent();
         }
+
+        private void iconPictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panelRedondeado13_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
