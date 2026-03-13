738ee352354d81f72bc305bfb9f0e42d2e25da47
Amelia <you@example.com>
2026-03-13 16:28:43 -0600

    SecurityNearFinish

--- Views/Iluminacion.Designer.cs
+++ Views/Iluminacion.Designer.cs
@@ -28,25 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(363, 214);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Iluminacion";
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // Iluminacion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Iluminacion";
             this.Text = "Iluminacion";
             this.ResumeLayout(false);
@@ -54,7 +43,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
